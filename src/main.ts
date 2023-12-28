3de982e220da72fcb7baa4a80a7f423cfa29c5b5
张雨凡 <39397772+ynzy@users.noreply.github.com>
2023-12-28 16:16:01 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import store from './stores';
+
+import App from './App.vue';
+import router from './router';
+// 引入全局样式
+import '@/styles/index.scss';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.mount('#app');
